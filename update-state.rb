c2f997dc71ce4a44d46411257896332e182e2c2b
Denis <dozdranagon@gmail.com>
2015-04-18 15:16:00 +0200

    no message

--- update-state.rb
+++ update-state.rb
@@ -6,17 +6,25 @@ require 'json'
 
 class Main
 	def self.run
+		pause_time = 5
+		
 		while(true) do
+			puts "Updating sequence...\n"
 			data = OpenStruct.new(JSON.load(open('http://likebox.es/animations/1.json')))
-			puts "Setting sequence to: #{data.file}"
+			puts "Setting sequence to: #{data.file}\n"
 			
 			open('img/current_sequence.gif', 'wb') do |file|
 			  file << open(data.file).read
 			end
 			
-			`./led-image-viewer -r16 img/current_sequence.gif`
+			puts "Killing previous instance..."
+			`pkill -f current_sequence.gif`
 			
-			sleep 10
+			puts "Launching led-image-viewer...\n";	
+			`./led-image-viewer -r16 img/current_sequence.gif`
+			puts "Waiting... #{pause_time} seconds\n";
+			
+			sleep pause_time
 		end
 	end
 end
